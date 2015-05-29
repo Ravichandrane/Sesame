406374db19468c077793f00022a42476dd13dc2a
Ravichandrane <rajendran.ravichandrane@gmail.com>
2015-05-29 10:13:46 +0200

    View Open/Close door

--- Android/app/src/main/java/fr/ravichandrane/sesame/MainActivity.java
+++ Android/app/src/main/java/fr/ravichandrane/sesame/MainActivity.java
@@ -1,6 +1,7 @@
 package fr.ravichandrane.sesame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 
@@ -10,6 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent loginIntent = new Intent(this, CloseMessageActivity.class);
+        startActivity(loginIntent);
     }
 
 }
---
+++ Android/app/src/main/java/fr/ravichandrane/sesame/OpenMessageActivity.java
@@ -0,0 +1,14 @@
+package fr.ravichandrane.sesame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class OpenMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_message);
+    }
+}
